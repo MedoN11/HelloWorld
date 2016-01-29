1b7ddfa5c4079e03addc875172c9203110a0aaa2
MedoN11 <medonasser951@yahoo.com>
2016-01-29 21:26:47 +0200

    Hello World++

---
+++ HelloWorld.cpp
@@ -0,0 +1,33 @@
+#include<fstream>
+#include<algorithm>
+#include <iostream>
+#include<numeric>
+#include<utility>
+#include<functional>
+#include<stdio.h>
+#include<assert.h>
+#include<memory.h>
+#include<bitset>
+#include <stack>
+#include <queue>
+#include <iterator>
+#include <vector>
+#include <iterator>
+#include <math.h>
+#include<cstring>
+#include <cmath>
+#include <sstream>
+#include<cstring>
+#include<cstdlib>
+#include <set>
+#include <cstdio>
+#include<map>
+#include <cmath>
+#include <map>
+#include "HelloWorld.h"
+using namespace std;
+
+int main()
+{
+    std::cout <<"HelloWorld";
+}
